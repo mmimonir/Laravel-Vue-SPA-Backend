10cef4a4cd94b7a65559bbd800f1a0dc47191e75
mmimonir <monirdhk.it@gmail.com>
2020-01-09 01:01:24 +0600

    Laravel API

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(UserLoginRequest $request)
+    {
+        $passwordGrantClient = Client::where('password_client', 1)->first();
+        $data =[
+            'grant_type'=>'password',
+            'client_id'=>$passwordGrantClient->id,
+            'client_secret'=>$passwordGrantClient->secret,
+            'username'=>$request->email,
+            'password'=>$request->password,
+            'scope'=>'*'
+
+        ];
+        $tokenRequest = Request::create('/oauth/token', 'post', $data);
+        return app()->handle($tokenRequest, $request->email);
+    }
+    // public function login(Request $request)
+    // {
+    //     $validatedData = $request->validate([
+    //     'email' => 'required|email',
+    //     'password' => 'required',
+    // ]);
+
+    //     $email = $request->email;
+    //     $password = $request->password;
+
+    //     $user = User::where('email', '=', $email)->first();
+
+    //     if (!$user) {
+    //         return response()->json(['success'=>false, 'message' => 'Login Fail, please check email id']);
+    //     }
+    //     if (!Hash::check($password, $user->password)) {
+    //         return response()->json(['success'=>false, 'message' => 'Login Fail, pls check password']);
+    //     }
+    //     $token = $user->createToken('Laravel')->accessToken;
+    //     return response()->json(['token' => $token], 200);
+    // }
+
+
+
+    public function register(UserRegisterRequest $request)
+    {
+        $user = User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+        if (!$user) {
+            return response()->json(["success"=>false, "message"=>"Registration Failed"]);
+        }
+        return response()->json(["success"=>true, "message"=>"Registration Succeeded"]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login', 'Api\AuthController@login')->name('api.login');
+Route::post('/register', 'Api\AuthController@register')->name('api.register');
+
+// Route::get('/user', function (Request $request) {
+//     return response()->json(['success'=>true]);
+// });
